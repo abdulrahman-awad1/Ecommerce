d532fc81b6ce6fe7b62667b4a103f6efaa056f1c
abdulrahman-awad1 <aulrahman034@gmail.com>
2025-05-03 15:44:40 +0300

    second commit

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -19,26 +19,33 @@ class CategoriesController extends Controller
      */
     public function index(Request $request)
     {
-        //index دا خاص بالسيرش ف ال ف صفحة ال
-        $query = Category::query(); //c هتسيرش بالكويري الجاي من الفورم
-        $name = $request->query('name'); //name الفورم كان فيها
-        if ($name){
-            $query->where('name','LIKE',"%{$name}%");
-        }
-        $status = $request->query('status'); //select الفورم كان فيها
-        if ($status){
-            $query->where('status','=',"$status");
+        $query = Category::query();
+
+        $name = $request->query('name');
+        if ($name) {
+            $query->where('name', 'LIKE', "%{$name}%");
         }
 
-       // $categories=$query->paginate(3);
-        $categories=Category::with('parent') //h دا بديل الجوين عشان اعرض محتوي جدول عن طريق جدول اخر
-         //   ->filter($request->query())
-            ->withCount('products as product_number')//x استدعيها ف البليد عشان تظهر
-         ->orderBy('id')
+        $status = $request->query('status');
+        if ($status) {
+            $query->where('status', '=', $status);
+        }
+
+        $categories = $query
+            ->with('parent')
+            ->withCount('products as product_number')
+            ->orderBy('id')
             ->paginate(3);
 
-        return view('dashboard.categories.index',compact('categories'));
+        return response()->json([
+            'data' => $categories->items(),
+            'current_page' => $categories->currentPage(),
+            'last_page' => $categories->lastPage(),
+            'per_page' => $categories->perPage(),
+            'total' => $categories->total(),
+        ]);
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -46,22 +53,26 @@ class CategoriesController extends Controller
     public function create()
     {
         $parents = Category::all();
-        return view('dashboard.categories.create',compact('parents'));
+
+        return response()->json([
+            'data' => $parents
+        ]);
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-      //  $request->validate([
-       //     'name'=>'required|string|min:3',
-       //     'parent_id'=>'nullable|int|exists:categories,id',
-       //     'image'=>'nullable|image|max:1048576',/*|dimensions:min_width=10,max_height=100000*/ //image النوع صوره , max عباره عن حجم الصوره ب البايت
-       //     'status'=>'in:active,archived',
-            //c دي الاسماء الجايه من الفورم مش الداتا بيز
-            //blade كدا الايرور مش هيظهر, لازم اعرف متغير اليرور ف صفحة ال  validation  بمجرد ما عملت ال
-       // ]);
+        //  $request->validate([
+        //     'name'=>'required|string|min:3',
+        //     'parent_id'=>'nullable|int|exists:categories,id',
+        //     'image'=>'nullable|image|max:1048576',/*|dimensions:min_width=10,max_height=100000*/ //image النوع صوره , max عباره عن حجم الصوره ب البايت
+        //     'status'=>'in:active,archived',
+        //c دي الاسماء الجايه من الفورم مش الداتا بيز
+        //blade كدا الايرور مش هيظهر, لازم اعرف متغير اليرور ف صفحة ال  validation  بمجرد ما عملت ال
+        // ]);
 
         $user = new Category;
 
@@ -79,14 +90,14 @@ class CategoriesController extends Controller
 
         $data = $request->except('image'); //b هنا استثنيت ان الصوره تاخد قيمتها من الفورم لان هتاخد القيمه من المسار الجديد
 
-      /*  $image = $request->file('image');//  name كدا روحت ع الفورم دخلت جوه الحقل ال نوعه فيل واسمه
-       // $image_name =  $request->file('image')->getClientOriginalName();//m جبت الاسم الاصلي للملف
-        $path = $image->store('uploads','public'); //storage كل دا داخل ال  image مكان تخزين الصوره ف ملف  البابلك ف مجلد
-        //filesystem دا المسؤل عن مكان التخزين
-        //filesystem ادخل*/
+        /*  $image = $request->file('image');//  name كدا روحت ع الفورم دخلت جوه الحقل ال نوعه فيل واسمه
+         // $image_name =  $request->file('image')->getClientOriginalName();//m جبت الاسم الاصلي للملف
+          $path = $image->store('uploads','public'); //storage كل دا داخل ال  image مكان تخزين الصوره ف ملف  البابلك ف مجلد
+          //filesystem دا المسؤل عن مكان التخزين
+          //filesystem ادخل*/
         $data['image']=$this->upload_Image($request);
 
-      //  $data['image']=$this->upload_Image($request);
+        //  $data['image']=$this->upload_Image($request);
         //upload_image استدعاء فانكشن ال
 
 
@@ -94,18 +105,18 @@ class CategoriesController extends Controller
 
 
         $category = Category::create($data);
-        return redirect()->route('categories.index')
-            ->with('success','the product added'); // flash message دي رساله المفروض تظهر بعد عملية الاضافه تسمي
-                                                    // index هستقبل الرساله ف
+        return response()->json([
+            'message' => 'the product added'
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Category $category )
+    public function show(Category $category)
     {
-        return view('dashboard.categories.show',[
-            'category'=>$category
+        return response()->json([
+            'data' => $category
         ]);
     }
 
@@ -116,12 +127,18 @@ class CategoriesController extends Controller
     {
         $category = Category::findOrFail($id);
 
-        $parents = Category::where('id','<>',$id)
+        $parents = Category::where('id', '<>', $id)
             ->where(function ($query) use ($id) {
-                $query->whereNull('parent_id')->orWhere('parent_id','<>',$id);
+                $query->whereNull('parent_id')
+                    ->orWhere('parent_id', '<>', $id);
             })->get();
-        return view('dashboard.categories.edit',compact('category','parents'));
+
+        return response()->json([
+            'category' => $category,
+            'available_parents' => $parents
+        ]);
     }
+
 
     /**
      * Update the specified resource in storage.
@@ -138,12 +155,12 @@ class CategoriesController extends Controller
         ]);
         $category = Category::findOrFail($id); // x دي بتعمل ابديت
 
-      //  $category =new Category(); دي بتكريت اوبجيكت جديد من الكاتيجوري
-      //  $category->fill($request->all())->save(); نفس وظيفة الابديت
+        //  $category =new Category(); دي بتكريت اوبجيكت جديد من الكاتيجوري
+        //  $category->fill($request->all())->save(); نفس وظيفة الابديت
 
-       /* $category->name = $request->name; طريقه اخري
-        $category->description = $request->description;
-        $category->save();*/
+        /* $category->name = $request->name; طريقه اخري
+         $category->description = $request->description;
+         $category->save();*/
         $old_image = $category->image; // f بجيب الصوره القديمه عشان احذفها
 
 
@@ -160,19 +177,20 @@ class CategoriesController extends Controller
         }
 
 
-           /* $image = $request->file('image');
-            $image_name =  $request->file('image')->getClientOriginalName();
-            $path = $image->storeAs('uploads',$image_name,'public');*/
-           /* $image->getSize();
-            $image->getMimeType();
-            $image->getClientOriginalExtension();*/
+        /* $image = $request->file('image');
+         $image_name =  $request->file('image')->getClientOriginalName();
+         $path = $image->storeAs('uploads',$image_name,'public');*/
+        /* $image->getSize();
+         $image->getMimeType();
+         $image->getClientOriginalExtension();*/
 
 
 
 
+        return response()->json([
+            'message' => 'the product updated'
+        ], 200);
 
-        return redirect()->route('categories.index')
-            ->with('success','the product updated');
 
     }
 
@@ -185,14 +203,16 @@ class CategoriesController extends Controller
         $category->delete();
 
 
-     //   Category::destroy($id);
+        //   Category::destroy($id);
 
-        return redirect()->route('categories.index')
-            ->with('success','the product deleted');
+        return response()->json([
+            'message' => 'the product deleted'
+        ], 200);
+
 
     }
     protected function upload_Image(Request $request){
-       // $data = $request->except('image'); //b هنا استثنيت ان الصوره تاخد قيمتها من الفورم لان هتاخد القيمه من المسار الجديد
+        // $data = $request->except('image'); //b هنا استثنيت ان الصوره تاخد قيمتها من الفورم لان هتاخد القيمه من المسار الجديد
 
         if (!$request->hasFile('image')){
             return;
@@ -207,13 +227,19 @@ class CategoriesController extends Controller
 
     public function trash(){
         $categories = Category::onlyTrashed()->paginate();
-        return view('dashboard.categories.trash',compact('categories'));
+        return response()->json([
+            'data' => $categories
+        ]);
+
     }
 
     public function restore($id){
         $categories = Category::onlyTrashed()->findOrFail($id);
         $categories->restore();
-        return redirect('categories.restore')->with('success','category restore');
+        return response()->json([
+            'message' => 'category restored'
+        ], 200);
+
     }
     public function forceDelete($id){
         $categories = Category::onlyTrashed()->findOrFail($id);
@@ -222,7 +248,10 @@ class CategoriesController extends Controller
         if ($categories->image){
             storage::disk('public')->delete($categories->image);
         }
-        return redirect('categories.restore')->with('success','category deleted');
+        return response()->json([
+            'message' => 'category deleted'
+        ], 200);
+
     }
 
 
